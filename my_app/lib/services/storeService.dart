ccc9176e1d83a508fbc2fab786148d04ed44362e
Khari2003 <99896250+Khari2003@users.noreply.github.com>
2025-01-20 13:29:45 +0700

    sử dụng lại localhost

--- my_app/lib/services/storeService.dart
+++ my_app/lib/services/storeService.dart
@@ -5,8 +5,9 @@ import 'package:http/http.dart' as http;
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class StoreService {
+
   static Future<List<Map<String, dynamic>>> fetchStoresData() async {
-    final response = await http.get(Uri.parse('https://nha-hang-chay-8-b3a0efad7aea.herokuapp.com/store/getall'));
+    final response = await http.get(Uri.parse('https://secure-mesa-52472-4283a7099f0c.herokuapp.com//store/getall'));
     if (response.statusCode == 200) {
       return List<Map<String, dynamic>>.from(json.decode(response.body));
     } else {
