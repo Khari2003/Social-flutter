f5a2cc8c7288b5fd579207c52faaef1318e18c90
Nguyễn Xuân Thưởng <121345028+thuongerikdev@users.noreply.github.com>
2025-05-13 23:45:43 +0700

    update

--- my_app/lib/services/group/groupChatService.dart
+++ my_app/lib/services/group/groupChatService.dart
@@ -12,7 +12,7 @@ class GroupChatService extends ChangeNotifier {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
-  final String apiEndpoint = "http://192.168.1.200:5000/upload";
+  final String apiEndpoint = "http://192.168.30.53:5000/upload";
   final Authservice _authservice = Authservice();
 
   /// Upload ảnh lên Cloudinary
